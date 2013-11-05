d7679a1e3925c84298e3f2140f2475220f663f2e
matt <matt@acmn.com.au>
2013-11-05 11:27:33 +1100

    Initial Commit
    Signed-off-by: matt <matt@acmn.com.au>

---
+++ themes/response/footer.php
@@ -0,0 +1,16 @@
+</section><!-- Container End -->
+
+<div class="row full-width">
+	<?php dynamic_sidebar("Footer"); ?>
+</div>
+
+<footer class="row full-width" role="contentinfo">
+	<div class="small-12 large-4 columns">
+		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ themes/response/js/foundation.js
@@ -0,0 +1,26 @@
+/*
+  Other Elements:
+  // "foundation/foundation.alerts.js"
+  // "foundation/foundation.clearing.js"
+  // "foundation/foundation.cookie.js"
+  // "foundation/foundation.dropdown.js"
+  // "foundation/foundation.forms.js"
+  // "foundation/foundation.joyride.js"
+  // "foundation/foundation.magellan.js"
+  // "foundation/foundation.orbit.js"
+  // "foundation/foundation.reveal.js"
+  // "foundation/foundation.section.js"
+  // "foundation/foundation.tooltips.js"
+  // "foundation/foundation.interchange.js"
+  // "foundation/foundation.placeholder.js"
+  // "foundation/foundation.abide.js"
+*/
+
+
+// @codekit-prepend "foundation/foundation.js"
+
+// @codekit-prepend "foundation/foundation.topbar.js"
+
+$(document ).ready(function() {
+    $(document).foundation();
+});
