f24163cbc741291ae835c25bf58405d3c244b5b9
matt <matt@acmn.com.au>
2013-11-06 17:19:48 +1100

    Getting there
    Signed-off-by: matt <matt@acmn.com.au>

--- themes/response/footer.php
+++ themes/response/footer.php
@@ -6,7 +6,9 @@
 
 <footer class="row full-width" role="contentinfo">
 	<div class="small-12 large-4 columns">
+	<hr>
 		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+        <?php if(is_user_logged_in()){ echo get_num_queries(); } ?>
 	</div>
 </footer>
 
--- themes/response/front-page.php
+++ themes/response/front-page.php
@@ -3,23 +3,34 @@ get_header(); ?>
 
 <!-- Row for main content area -->
 	<div class="small-12 large-12 columns" role="main">
-    
-	<?php /* Start loop */ ?>
-	<?php while (have_posts()) : the_post(); ?>
+  
+  <?php
+  $args = array( 
+      'post_type' => 'event',  
+      );
+  
+  $the_query = new WP_Query( $args );
+   
+   if ( $the_query->have_posts() ) :
+    while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<a href="<?php the_permalink(); ?>"><h1 class="entry-title"><?php the_title(); ?></h1></a>
 			</header>
 			<div class="entry-content">
-				<?php the_content(); ?>
+				<?php the_excerpt(); ?>
+				<a href="<?php the_permalink(); ?>">View Event</a>
 			</div>
-			<footer>
-				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
-
-			</footer>
 		</article>
-	<?php endwhile; // End the loop ?>
+	<?php   
+  endwhile;
+  endif;
+  
+  // Reset Post Data
+  wp_reset_postdata();
+  
+  ?> 
 
 	</div>
-		
+		<br><br>
 <?php get_footer(); ?>
