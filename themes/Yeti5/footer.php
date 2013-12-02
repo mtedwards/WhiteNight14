dfcae1e796c1866939d55b7d2c0ab8bbb4903e69
Matt Edwards <matt@emptyhead.com.au>
2013-12-03 08:55:46 +1100

    Footer Work
    Signed-off-by: Matt Edwards <matt@emptyhead.com.au>

--- themes/Yeti5/footer.php
+++ themes/Yeti5/footer.php
@@ -1,143 +1,130 @@
   </div> <?php // main-content-section end ?>
- <footer class="row">
-  <div class="small-12 columns">
-    <div class="large-4 columns logos">
-    
+ <footer>
+  <div class="row">  
+    <div class="small-4 columns logos">
   <!-- Logos  / full programme /  journeys --> 
-  		<div class="large-4 columns">
-        <div class="logosPlace">
-          <img src="img/MEL_VA_REV_87.png" alt="Melbourne"><br><br>
-          <img src="img/Insignia-portrait_WHT_87.png" alt="Victoria"> </div>
-        </div>   
-    	<div class="small-8 columns">
-        <div class="logosNav">
-     <ul>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=art"><b>full program</b></a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=art"><b>journeys</b></a></li>
-         
-    </ul>
- </div>   
-</div> 
+      <div class="row">
+        <div class="small-4 columns">
+          <img src="<?php bloginfo('template_url') ?>/img/MEL_VA_REV_87.png" alt="Melbourne"><br><br>
+          <img src="<?php bloginfo('template_url') ?>/img/Insignia-portrait_WHT_87.png" alt="Victoria"> </div>
+        </div> 
+        <div class="small-8 columns">
+          <ul>
+               <li><a href="http://acmndev.com.au/whitenight/events/?genre=art"><b>full program</b></a></li>
+               <li><a href="http://acmndev.com.au/whitenight/events/?genre=art"><b>journeys</b></a></li>
+               
+          </ul>
+        </div>
+      </div> 
+    <div class="small-3 columns explore">
+        <div class="row">
+           <!-- Explore  menu--> 
+            <div class="small-6 columns">
+                <b>Precincts</b>
+                 <ul>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/01-northern-lights/">Northern Lights</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/02-lucky-dip/">Lucky Dip</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/03-jrb/">J+R&amp;B</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/04-shadows/">Shadows</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/04-the-vortex/">The Vortex</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/06-wonderland/">Wonderland</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/08-midden/">Midden</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/09-alex-and-the-engineer/">Alex and the engineer</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/10-tattooed-city/">Tattooed City</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/precinct/11-outer-limits/">Outer Limits</a></li>
+                </ul>
+            </div>
+            <div class="small-6 columns">
+                 <ul>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=art">Art</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=design">Design</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=family">Family</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=fashion">Fashion</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=film">Film</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=lighting">Lighting</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=music">Music</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=performance">Performance</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/events/?genre=sport">Sport</a></li>
+                </ul>
+            </div>
+        </div>
+      </div><?php //end explore ?>
+    <!-- my night menu  menu--> 
+    <div class="small-1 columns myNight">
+          <b>my night</b>
+           <ul>
+               <li><a href="#">Create my night</a></li>
+               <li><a href="#">Login</a></li>
+               <li><a href="#">help</a></li>
+          </ul>
+      </div><?php //end myNight ?>
+    <div class="small-4 columns search">
+        <div class="row">
+          <div class="small-6 columns ">
+            <b>visitor info</b>
+                 <ul>
+                     <li><a href="#">Visiting Melbourne</a></li>
+                     <li><a href="#">Gettting Here</a></li>
+                     <li><a href="#">Getting Around</a></li>
+                     <li><a href="#">Map</a></li>
+                     <li><a href="http://acmndev.com.au/whitenight/where-to-eat-rest/">Where to Eat &amp; Rest</a></li>
+                     <li><a href="#">Getting Home</a></li>
+            
+                </ul>
+            </div>
+          <div class=" small-6 columns ">
+          <b>about us</b>
+               <ul><li><a href="http://acmndev.com.au/whitenight/contact-us/">Contact Us</a></li>
+                   <li><a href="#">History</a></li>
+                   <li><a href="#">International Events</a></li>
+                   <li><a href="#">The Team</a></li>
+                   <li><a href="#">Partners/Supporters</a></li>
+          
+              </ul>
+          </div>
+        </div>
+      </div>
+  </div><?php //end row ?>
     
-    
-  </div>  
-    
-  <div class="large-3 columns explore">
- <!-- Explore  menu--> 
-<div class="small-6 columns">
-<div class="footerMenuTitle">
-<b>Precincts</b>
- </div>
-     <ul>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/01-northern-lights/">Northern Lights</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/02-lucky-dip/">Lucky Dip</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/03-jrb/">J+R&amp;B</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/04-shadows/">Shadows</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/04-the-vortex/">The Vortex</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/06-wonderland/">Wonderland</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/08-midden/">Midden</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/09-alex-and-the-engineer/">Alex and the engineer</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/10-tattooed-city/">Tattooed City</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/precinct/11-outer-limits/">Outer Limits</a></li>
-    </ul>
-</div>
-<div class="small-6 columns">
-     <ul>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=art">Art</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=design">Design</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=family">Family</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=fashion">Fashion</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=film">Film</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=lighting">Lighting</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=music">Music</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=performance">Performance</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/events/?genre=sport">Sport</a></li>
-    </ul>
-</div>
-
-
-</div>
-<!-- my night menu  menu--> 
-<div class="large-1 columns myNight">
   
-<div class="footerMenuTitle">
-<b>my night</b>
- </div>
-     <ul>
-         <li><a href="#">Create my night</a></li>
-         <li><a href="#">Login</a></li>
-         <li><a href="#">help</a></li>
-      
-    </ul>
-</div>
 
-  <div class="large-4 columns search">
+
+
+
+
   <!-- search  menu / visitor info / about us/ social icons / form--> 
-<div class="row" > 
-<div class="small-6 columns ">
-<div class="footerMenuTitle">
-<b>visitor info</b>
- </div>
-     <ul>
-         <li><a href="#">Visiting Melbourne</a></li>
-         <li><a href="#">Gettting Here</a></li>
-         <li><a href="#">Getting Around</a></li>
-         <li><a href="#">Map</a></li>
-         <li><a href="http://acmndev.com.au/whitenight/where-to-eat-rest/">Where to Eat &amp; Rest</a></li>
-         <li><a href="#">Getting Home</a></li>
 
-    </ul>
-</div>
-<div class=" small-6 columns ">
-<div class="footerMenuTitle">
-<b>about us</b>
- </div>
-     <ul><li><a href="http://acmndev.com.au/whitenight/contact-us/">Contact Us</a></li>
-         <li><a href="#">History</a></li>
-         <li><a href="#">International Events</a></li>
-         <li><a href="#">The Team</a></li>
-         <li><a href="#">Partners/Supporters</a></li>
 
-    </ul>
-</div>
-</div>
 <div class="row footerWhite">
-
-<div class="small-8 columns">
-<div class="socialBox"><b>Share<br>white night<br>Melbourne</b></div>
-</div>
-<div class="small-4 columns">
-<div class="socialFooterIcons">facebook<br>
-tweeter<br>google
-</div>
-</div>
-</div>
-<div class="row">
-<div class="small-12 columns footerBlack" >
-<div class="small-6 footerSubscribe">subscribe</div>
-<div class="small-6 footerForm"><form> <input type="text" id="right-label" placeholder="Email Address"></form></div>
-</div>
-</div>
-
-
+  <div class="small-8 columns">
+    <div class="socialBox"><b>Share<br>white night<br>Melbourne</b></div>
   </div>
+  <div class="small-4 columns">
+    <div class="socialFooterIcons">facebook<br>
+                  tweeter<br>google
+    </div>
   </div>
-<div class="row">
- <div class="large-12 columns">
- <div class="small-6 columns footerCopyrights">
- Copyrights WNM 2014
- </div>
- 
-  <div class="small-6 columns footerPrivacy">
- <a href="#">Privacy Policy</a><br>
-Website by <a href="#">ACMN</a>
+</div>
+  <div class="row">
+    <div class="small-12 columns footerBlack" >
+      <div class="small-6 footerSubscribe">subscribe</div>
+      <div class="small-6 footerForm"><form> <input type="text" id="right-label" placeholder="Email Address"></form></div>
+    </div>
+  </div>
+  </div>    
 </div>
 
-</div>
- 
+<div class="row">
+  <div class="small-6 columns">Copyrights WNM 2014</div>
+  <div class="small-6 columns right">
+     <a href="#">Privacy Policy</a><br>
+     Website by <a href="#">ACMN</a>
+  </div>
 </div> 
  
-</footer> </div><!-- Container End -->
+</footer> 
+
+</div><!-- Container End -->
 <script type="text/javascript">
   window.___gcfg = {lang: 'en-GB'};
 
