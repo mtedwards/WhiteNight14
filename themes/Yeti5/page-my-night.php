9f717492d8a12b965d03da4405fee3136e85da56
Matt <matt@emptyhead.com.au>
2014-02-06 12:19:57 +1100

    Add Log out button to My Night Page
    Signed-off-by: Matt <matt@emptyhead.com.au>

--- themes/Yeti5/page-my-night.php
+++ themes/Yeti5/page-my-night.php
@@ -154,7 +154,35 @@
   	<?php endwhile; // End the loop ?>
 	</div>
   <div class="small-12 medium-4 large-4 columns is-single-page featured-info">
-      <?php include(locate_template('partials/sidebar-details.php')); ?>
+      <div class="centered-text date-text hide-for-medium-down white-bg">
+        <h2>22 February 2014</h2>
+        <h3 class="no-bottom">7PM TO 7AM</h3>
+      </div>
+      <div class="centered-text countdown hide-for-medium-down white-bg">
+        <h6>Countdown to White Night Melbourne</h6>
+        <div id="countdown"></div>
+      </div>
+      <div class="social centered-text white-bg">
+        <a href="https://www.facebook.com/WhiteNightMelbourne" class="social-icon facebook" target="_blank" onclick="_gaq.push(['_trackEvent', 'click', 'social', 'facebook'])" >Like us on Facebook</a>
+        <a href="https://twitter.com/whitenightmelb" class="social-icon twitter" target="_blank" onclick="_gaq.push(['_trackEvent', 'click', 'social', 'twitter'])">Follow us on Twitter</a>
+        <a href="http://instagram.com/whitenightmelb/" class="social-icon instagram" target="_blank" onclick="_gaq.push(['_trackEvent', 'click', 'social', 'instagram'])">Follow us on Instagram</a>
+        <a href="https://www.youtube.com/user/WhiteNightMelbourne" class="social-icon youtube" target="_blank" onclick="_gaq.push(['_trackEvent', 'click', 'social', 'youtube'])">Join our YouTUbe Channel</a>
+        <a href="https://plus.google.com/109173756154545584645" class="social-icon gplus" target="_blank" onclick="_gaq.push(['_trackEvent', 'click', 'social', 'gplus'])">Join us on Google +</a>
+        <h2>#WHITENIGHTMELB</h2>
+      </div>
+      <div class="hide-for-medium-down centered-text">
+        <?php $myNightEvents = upb_list_bookmarks(); ?>
+        <?php if(! $myNightEvents) { ?>
+          <a href="#" class="openMyNight"><img src="<?php bloginfo('template_url'); ?>/img/planningMN2.jpg"></a>
+        <?php } ?>
+      </div>
+      
+      <?php if ( is_user_logged_in() ) { ?>
+        <div class="social centered-text white-bg">
+          <a class="button blue expand padding-bottom logout" href="<?php echo wp_logout_url(get_bloginfo('url')); ?>" title="Logout">Logout</a>
+        </div>
+      <?php } ?>
+      
   </div>
   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 <?php get_footer(); ?>
