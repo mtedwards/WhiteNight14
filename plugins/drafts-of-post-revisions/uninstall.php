61ee86334d0cd2b2ce9881a0bf76ff9d4cf61b93
Matt Edwards <matt@emptyhead.com.au>
2014-07-02 10:35:16 +1000

    Untrack Plugins
    Signed-off-by: Matt Edwards <matt@emptyhead.com.au>

--- plugins/drafts-of-post-revisions/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-// this seems to be the preferred way to handle an uninstall
-if ( !defined('WP_UNINSTALL_PLUGIN' ) )
-	exit();
-
-require_once('core.php');
-
-Draft_Post_Revisions::uninstall();
